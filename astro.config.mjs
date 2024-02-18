f891d5b433e35cfd75de35cce8cf91d33fff3b7d
David Zavala <davidzavalaugalde@gmail.com>
2024-02-17 19:41:55 -0600

    Changes were made for deployment to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+   site: ' https://davidzavala123.github.io',
+   base: '/lombok-webpage' 
+});
