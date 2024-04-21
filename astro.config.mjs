5016090da7f445df9b3e687d7cae54ec5169e4bb
David Zavala <davidzavalaugalde@gmail.com>
2024-04-21 15:00:37 -0600

    netlify configurations were added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-   site: ' https://davidzavala123.github.io',
-   base: '/lombok-webpage' 
+  site: ' https://davidzavala123.github.io',
+  base: '/lombok-webpage',
+  output: "server",
+  adapter: netlify()
 });
